594d78670bbc3788bd928ea082b26a921a31b59a
Marchites <gilangaryadimahardika@gmail.com>
2026-06-05 16:20:31 +0700

    fix: fix image display not appearing on add property page

--- resources/views/dashboard/properties/index.blade.php
+++ resources/views/dashboard/properties/index.blade.php
@@ -29,11 +29,12 @@
 
             <div style="width:80px; height:80px; border-radius:10px; overflow:hidden; background:#eee;">
 
-                <div class="property-img"
-                    style="background-image:url('{{ $property->images->first()?->image_path
+                <img src="{{ $property->images->first()?->image_path
                                 ? asset('storage/' . $property->images->first()->image_path)
-                                : 'https://images.unsplash.com/photo-1512917774080-9991f1c4c750?q=80&w=1470&auto=format&fit=crop' }}')">
-                </div>
+                                : 'https://images.unsplash.com/photo-1512917774080-9991f1c4c750?q=80&w=1470&auto=format&fit=crop' }}"
+                    style="width:100%; height:100%; object-fit:cover;">
+
+                    
 
             </div>
 
